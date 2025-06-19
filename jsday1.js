0cc00d649e4fec63ffdc314f471bf78f5ae90955
HARIRAMRAMDASS <hariramr.23auto@kongu.edu>
2025-06-19 16:05:05 +0530

    third

---
+++ jsday1.js
@@ -0,0 +1,8 @@
+let a = 10
+let b = '20'
+let c = true
+let d = null
+let e = 3.14
+let f = 32165854851451451453564525
+console.log(a**2)
+console.log(2*e*a)
---
+++ operator.js
@@ -0,0 +1,49 @@
+let a = 10
+let b = 10
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
+console.log(++a)
+console.log(--b)
+console.log(a++)
+console.log(b--)
+console.log(a+=b)
+console.log(a-=b)
+console.log(a*=b)
+console.log(a/=b)
+console.log(a%=b)
+console.log(a**=b)
+console.log(a==b)
+console.log(a!=b)
+console.log(a===b)
+console.log(a!==b)
+console.log(a<b)
+console.log(a>b)
+console.log(a<=b)
+console.log(a>=b)
+console.log(a&&b)
+console.log(a||b)
+console.log(!a)
+console.log(!b)
+console.log(a&b)
+console.log(a|b)
+console.log(a^b)
+console.log(~a)
+console.log(a<<b)
+console.log(a>>b)
+console.log(a>>>b)
+console.log((a==b)?"yes":"no")
+if(a==b) {
+    console.log("a is equal to b")
+}
+else if(a!=b) {
+    console.log("a is not equal to b")
+}
+else {
+    console.log("a is not equal to b")
+}
+
+
